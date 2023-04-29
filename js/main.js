f672cc25b090f9ee95774da7edaeb17c6e8b3554
tijeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-04-28 23:12:36 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('Working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://ill-tan-pigeon-garb.cyclic.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
+
